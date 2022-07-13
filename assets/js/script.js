59acded54b71b3306c03399f3cd676315eae66e7
ilcary <carisiodigiampietro54@gmail.com>
2022-07-13 16:11:10 +0200

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,25 @@ var swiper = new Swiper(".slide-content", {
   },
 });
 
+/*==========================================menu hamburger=====================================*/
+
+function plsStick(elemento){
+  let getSticky = document.querySelector(elemento);
+  let headerHeight = document.querySelector('#bgheader').clientHeight;
+  let navHeight = document.querySelector('#home').clientHeight;
+  let correctHeight = headerHeight - navHeight;
+
+  if(document.body.scrollTop > correctHeight && 
+      !getSticky.classList.contains('sticked')){
+      getSticky.classList.add('sticked');
+  }else if(document.body.scrollTop < correctHeight - 1){
+      getSticky.classList.remove('sticked');
+  }
+  console.log(getSticky)
+  console.log(document.body.scrollTop)
+}
+
+
 /*==============================================ANIMAZIONI========MENU=============================*/
 
 function reveal() {
@@ -51,20 +70,3 @@ function reveal() {
 window.addEventListener("scroll", reveal);
 
 
-/*==========================================menu hamburger=====================================*/
-
-function plsStick(elemento){
-  let getSticky = document.querySelector(elemento);
-  let headerHeight = document.querySelector('#bgheader').clientHeight;
-  let navHeight = document.querySelector('#top-nav').clientHeight;
-  let correctHeight = headerHeight - navHeight;
-
-  if(document.body.scrollTop > correctHeight && 
-      !getSticky.classList.contains('sticked')){
-      getSticky.classList.add('sticked');
-  }else if(document.body.scrollTop < correctHeight - 1){
-      getSticky.classList.remove('sticked');
-  }
-  console.log(getSticky)
-  console.log(document.body.scrollTop)
-}
